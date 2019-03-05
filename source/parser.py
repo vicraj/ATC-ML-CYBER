5e02256c724338acdd1c015987944399b5cd2840
Igor Mekhtiev <imehdi@gmail.com>
2019-03-04 20:03:44 -0600

    Multithreaded splitter

--- source/parser.py
+++ source/parser.py
@@ -3,7 +3,25 @@ from IPython.display import display, HTML
 from datetime import datetime
 from datetime import timedelta
 import subprocess
+import re
+from multiprocessing.dummy import Pool as ThreadPool
 
+def run_command(command):
+    print("Starting command: " + command)
+    subprocess.run(command, shell=True)
+    print("Finished command: " + command)
+
+def get_valid_filename(s):
+    """
+    Return the given string converted to a string that can be used for a clean
+    filename. Remove leading and trailing spaces; convert other spaces to
+    underscores; and remove anything that is not an alphanumeric, dash,
+    underscore, or dot.
+    >>> get_valid_filename("john's portrait in 2004.jpg")
+    'johns_portrait_in_2004.jpg'
+    """
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
 
 def main():
     df = pd.read_csv('tcpdump.list', delim_whitespace=True)
@@ -19,6 +37,9 @@ def main():
     # Output this table into an html file.
     df2.to_html('filename.html')
 
+    commands = []
+
+    print("Generate commands to run...")
     for index, row in df2.iterrows():
         datetime_start = datetime.strptime(("%s %s") % (row['date'], row['time']), '%m/%d/%Y %H:%M:%S')
         duration = row['duration'].split(':')
@@ -27,20 +48,26 @@ def main():
         seconds = int(duration[2])
         datetime_end = datetime_start + timedelta(hours=hours, minutes=minutes, seconds=seconds)
 
-        #print(row['attack_name'], datetime_start, datetime_end)
-        #print()
         #editcap -v  -A "1998-01-23 16:03:52" -B "1998-01-23 16:03:58"  sample_data01.tcpdump /dev/null
         file_name = "%s_%s.pcap" % (row['id'], row['attack_name'])
-        command = ("editcap -v -A \"%s\" -B \"%s\"  sample_data01.tcpdump output/%s") % (datetime_start, datetime_end, file_name)
-        #command = ("editcap -v -A \"%s\" -B \"%s\"  ../sample_data/sample_data01.tcpdump /dev/null") % ("1998-01-23 16:03:52", datetime_end)
+        file_name = get_valid_filename(file_name)
+        command = ("editcap -A \"%s\" -B \"%s\"  sample_data01.tcpdump output/%s") % (datetime_start, datetime_end, file_name)
 
-
-        #command = "editcap -v  -A \"1998-01-23 16:03:52\" -B \"1998-01-23 16:03:58\"  ../sample_data/sample_data01.tcpdump /dev/null"
-        print(command)
+        #print(command)
 
         #print("'%s'" % datetime_start)
+        commands.append(command)
 
-        subprocess.run(command, shell=True)
+    print("Starting Multithreaded Splitting...")
+    pool = ThreadPool(8)
+
+    # split datasets in their own threads
+    # and return the results
+    pool.starmap(run_command, zip(commands))
+
+    # close the pool and wait for the work to finish
+    pool.close()
+    pool.join()
 
 
 if __name__ == "__main__":
