0f491ff01672c1fcd88921e168b87f21eb60c339
Igor Mekhtiev <igor@m3ki.net>
2019-04-16 10:35:54 -0500

    WIP Parsing PCAP with python

---
+++ source/pcap_dpkt.py
@@ -0,0 +1,101 @@
+
+"""
+Use DPKT to read in a pcap file and print out the contents of the packets
+This example is focused on the fields in the Ethernet Frame and IP packet
+"""
+import dpkt
+import datetime
+import socket
+from dpkt.compat import compat_ord
+
+
+def mac_addr(address):
+    """Convert a MAC address to a readable/printable string
+
+       Args:
+           address (str): a MAC address in hex form (e.g. '\x01\x02\x03\x04\x05\x06')
+       Returns:
+           str: Printable/readable MAC address
+    """
+    return ':'.join('%02x' % compat_ord(b) for b in address)
+
+
+
+def inet_to_str(inet):
+    """Convert inet object to a string
+
+        Args:
+            inet (inet struct): inet network address
+        Returns:
+            str: Printable/readable IP address
+    """
+    # First try ipv4 and then ipv6
+    try:
+        return socket.inet_ntop(socket.AF_INET, inet)
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, inet)
+
+
+
+def print_packets(pcap):
+    """Print out information about each packet in a pcap
+
+       Args:
+           pcap: dpkt pcap reader object (dpkt.pcap.Reader)
+    """
+    # For each packet in the pcap process the contents
+    for timestamp, buf in pcap:
+
+        # Print out the timestamp in UTC
+        print('Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
+
+        # Unpack the Ethernet frame (mac src/dst, ethertype)
+        eth = dpkt.ethernet.Ethernet(buf)
+        print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
+
+        # Make sure the Ethernet data contains an IP packet
+        if not isinstance(eth.data, dpkt.ip.IP):
+            print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+            continue
+
+        # Now unpack the data within the Ethernet frame (the IP packet)
+        # Pulling out src, dst, length, fragment info, TTL, and Protocol
+        ip = eth.data
+
+        # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+        do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+        more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+        fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+
+        # Extract TCP Flags
+        if ip.p==dpkt.ip.IP_PROTO_TCP:
+            tcp = ip.data
+            fin_flag = (tcp.flags & dpkt.tcp.TH_FIN)  != 0
+            syn_flag = (tcp.flags & dpkt.tcp.TH_SYN)  != 0
+            rst_flag = (tcp.flags & dpkt.tcp.TH_RST)  != 0
+            psh_flag = (tcp.flags & dpkt.tcp.TH_PUSH) != 0
+            ack_flag = (tcp.flags & dpkt.tcp.TH_ACK)  != 0
+            urg_flag = (tcp.flags & dpkt.tcp.TH_URG)  != 0
+            ece_flag = (tcp.flags & dpkt.tcp.TH_ECE)  != 0
+            cwr_flag = (tcp.flags & dpkt.tcp.TH_CWR)  != 0
+            print ("Packet is urgent")
+            print(urg_flag)
+
+
+        # Print out the info
+        # print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
+        #       (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
+
+
+
+
+def test():
+    """Open up a test pcap file and print out the packets"""
+    with open('../sample_data/sample_data01.tcpdump', 'rb') as f:
+        pcap = dpkt.pcap.Reader(f)
+        print_packets(pcap)
+
+
+
+if __name__ == '__main__':
+    test()
