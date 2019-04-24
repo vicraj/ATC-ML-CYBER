ab677e5e3decd3595aa8dd4074324dacadf8c693
Igor Mekhtiev <igor@m3ki.net>
2019-04-24 11:46:22 -0500

    Rough implementation complete

--- source/pcap_dpkt.py
+++ source/pcap_dpkt.py
@@ -14,6 +14,7 @@ from datetime import timezone
 from IPython.display import display, HTML
 import sys
 import pytz
+from multiprocessing.pool import ThreadPool
 
 
 def mac_addr(address):
@@ -60,7 +61,7 @@ def extract_packet_data(pcap, event_metadata):
 
        Args:
            pcap: dpkt pcap reader object (dpkt.pcap.Reader)
-           metadata_list: python list collection of dictionary objects containing metadata in the following format:
+           event_metadata: python list collection of dictionary objects containing metadata in the following format:
            [{'id': id,
              'start': datetime_start,
              'end': datetime_end,
@@ -71,18 +72,27 @@ def extract_packet_data(pcap, event_metadata):
              ...
              },....]
     """
-    print('Starting to parse EVENT...')
+    # print('Starting to parse EVENT...')
     # print('Events to be considered(From Metadata): %s' % len(metadata_list))
     # For each packet in the pcap process the contents
-    parsing_event = False
-    # metadata_list.reverse()
-    # event_metadata = metadata_list.pop(0)
-    num_parsed = 0
 
     event_start = event_metadata['start']
     event_end = event_metadata['end']
     event_id = event_metadata['id']
     attack_name = event_metadata['attack_name']
+
+    csv_metadata = {'duration': event_metadata['duration'],
+                    'service': event_metadata['service'],
+                    'attack_name': event_metadata['attack_name'],
+                    'num_fin_flag': 0,
+                    'num_rst_flag': 0,
+                    'num_syn_flag': 0,
+                    'num_psh_flag': 0,
+                    'num_ack_flag': 0,
+                    'num_urg_flag': 0,
+                    'num_ece_flag': 0,
+                    'num_cwr_flag': 0
+                    }
 
     for timestamp, buf in pcap:
         current_timestamp = datetime.utcfromtimestamp(timestamp)
@@ -90,41 +100,14 @@ def extract_packet_data(pcap, event_metadata):
         if current_timestamp < event_start:
             continue
 
-
-        # event_metadata = get_event_by_timestamp(current_timestamp, metadata_list)
-        # if event_metadata is None:
-        #     continue
-
-
         event_start = event_metadata['start']
         event_end = event_metadata['end']
         event_id = event_metadata['id']
 
-        print("Processing event: ", event_id)
+        # print("Processing event: ", event_id)
         # Print out the timestamp in UTC
-
-        # print(current_timestamp)
-        # print(event_start)
-        # print(event_end)
-        # print('-----')
         #print('Timestamp: ', str(datetime.utcfromtimestamp(timestamp)))
-        # print(timestamp)
-        # if current_timestamp < event_start:
-        #     continue
-        # if current_timestamp >= event_start and current_timestamp <= event_end:
-        #     parsing_event = True
-        #     print ("Parsing Event id #", event_id)
-        #      # sys.exit()
-        # else:# parsing_event == True:
-        #     if len(metadata_list) <= 0:
-        #         continue
-        #     num_parsed += 1
-        #     print("POP dat stack", num_parsed)
-        #     parsing_event = False
-        #     event_metadata = metadata_list.pop(0)
-        #
-        #     event_start = event_metadata['start']
-        #     event_end = event_metadata['end']
+
 
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
@@ -147,23 +130,27 @@ def extract_packet_data(pcap, event_metadata):
         # Extract TCP Flags
         if ip.p==dpkt.ip.IP_PROTO_TCP:
             tcp = ip.data
-            fin_flag = (tcp.flags & dpkt.tcp.TH_FIN)  != 0
-            syn_flag = (tcp.flags & dpkt.tcp.TH_SYN)  != 0
-            rst_flag = (tcp.flags & dpkt.tcp.TH_RST)  != 0
-            psh_flag = (tcp.flags & dpkt.tcp.TH_PUSH) != 0
-            ack_flag = (tcp.flags & dpkt.tcp.TH_ACK)  != 0
-            urg_flag = (tcp.flags & dpkt.tcp.TH_URG)  != 0
-            ece_flag = (tcp.flags & dpkt.tcp.TH_ECE)  != 0
-            cwr_flag = (tcp.flags & dpkt.tcp.TH_CWR)  != 0
+
+            csv_metadata['num_fin_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_FIN)  != 0 else 0)
+            csv_metadata['num_rst_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_RST)  != 0 else 0)
+            csv_metadata['num_syn_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_SYN)  != 0 else 0)
+            csv_metadata['num_psh_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_PUSH)  != 0 else 0)
+            csv_metadata['num_ack_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_ACK)  != 0 else 0)
+            csv_metadata['num_urg_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_URG)  != 0 else 0)
+            csv_metadata['num_ece_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_ECE)  != 0 else 0)
+            csv_metadata['num_cwr_flag'] += (1 if (tcp.flags & dpkt.tcp.TH_CWR)  != 0 else 0)
             # print ("Packet is urgent")
             # print(urg_flag)
 
 
+        # print (csv_metadata)
         # Print out the info
         # print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
         #       (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
         if current_timestamp > event_end:
             break
+
+    return csv_metadata
 
 
 def generate_attack_collection(file_name):
@@ -235,19 +222,41 @@ def generate_attack_collection(file_name):
     print('Done.')
     return metadata
 
+def parse_pcap_and_extract_data(event_metadata):
+    with open('sample_data01.tcpdump', 'rb') as f:
+        pcap = dpkt.pcap.Reader(f)
+        return extract_packet_data(pcap, event_metadata)
+
 
 def test():
     metadata_list = generate_attack_collection('../sample_data/tcpdump.list')
 
-    for event_metadata in metadata_list:
+    # for event_metadata in metadata_list:
+    #
+    #     """Open up a test pcap file and print out the packets"""
+    #     # with open('../sample_data/sample_data01.tcpdump', 'rb') as f:
+    #     with open('sample_data01.tcpdump', 'rb') as f:
+    #         pcap = dpkt.pcap.Reader(f)
+    #         extract_packet_data(pcap, event_metadata)
 
-        """Open up a test pcap file and print out the packets"""
-        # with open('../sample_data/sample_data01.tcpdump', 'rb') as f:
-        with open('sample_data01.tcpdump', 'rb') as f:
-            pcap = dpkt.pcap.Reader(f)
-            extract_packet_data(pcap, event_metadata)
+    print("Threads start....")
+    pool = ThreadPool(4)
+    # We need to zip together the two lists because map only supports calling functions
+    # with one argument. In Python 3.3+, you can use starmap instead.
+    results = pool.starmap(parse_pcap_and_extract_data, zip(metadata_list))
 
+    # print results
+    # print(results)
+    pool.close()
+    pool.join()
 
+    print("Wrinting CSV")
+    for result in results:
+        print(result)
 
+    df = pd.DataFrame(results)
+    df.to_csv("test.csv")
+
+    print("The end.")
 if __name__ == '__main__':
     test()
